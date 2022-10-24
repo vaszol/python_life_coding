d85adf52d749bd6c2430f49ce491745f6d83fd27
vaszol <vaszol@mail.ru>
2022-10-24 09:57:04 +0300

    PopMatrix rotate

--- start.py
+++ start.py
@@ -5,6 +5,15 @@ kv = '''
 FloatLayout:
     Button:
         text: 'Hello World!'
+        size_hint: None, None
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        canvas.before:
+            PushMatrix
+            Rotate:
+                angle: 45
+                origin: self.center
+        canvas.after:
+            PopMatrix
 '''
 
 class SuperApp(App):
