d06e4706b56a11ecc638064f90c4b1937b4589ee
vaszol <vaszol@mail.ru>
2022-10-25 21:14:33 +0300

    minecraft example

---
+++ minecraft.py
@@ -0,0 +1,35 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+
+class Voxel(Button):
+    def __init__(self, position=(0,0,0)):
+        super().__init__(
+            parent = scene,
+            position = position,
+            model = 'cube',
+            origin_y = .5,
+            texture = 'white_cube',
+            color = color.color(0, 0, random.uniform(.9, 1.0)),
+            highlight_color = color.lime,
+        )
+    def input(self, key):
+        pass
+
+for z in range(8):
+    for x in range(8):
+        voxel = Voxel(position=(x,0,z))
+
+
+def input(key):
+    if key == 'left mouse down':
+        hit_info = raycast(camera.world_position, camera.forward, distance=5)
+        if hit_info.hit:
+            Voxel(position=hit_info.entity.position + hit_info.normal)
+
+player = FirstPersonController()
+app.run()
+
+
+# https://github.com/pokepetter/ursina/blob/master/samples/minecraft_clone.py
